8a05fbe6ec5b33c229b9f17f3cfe6414796ed2dc
OmarFaruque96 <40633797+OmarFaruque96@users.noreply.github.com>
2019-06-24 18:07:39 +0600

    simple array example with count function (php-02)

---
+++ array.php
@@ -0,0 +1,15 @@
+<?php
+	
+	// array of fruits name
+	$fruits = array("apple","banana","berry","coconut","pineapple","orange","mango","wood apple","grapes","papaya");
+	
+	// to show the array
+	
+	//  print_r($fruits);
+	
+	// print the element with loop 
+	for($i=0;$i<count($fruits);$i++){
+		echo $fruits[$i] . "<br>";
+		
+	}
+?>
